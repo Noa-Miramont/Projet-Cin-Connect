b02a6b2b058b063c6066b880cf6fac4b54d29714
Noa-Miramont <noamiramont@gmail.com>
2026-04-02 20:24:07 +0200

    .

--- backend/src/utils/password.ts
+++ backend/src/utils/password.ts
@@ -2,7 +2,7 @@ const PASSWORD_POLICY_MESSAGE =
   'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.'
 
 const PASSWORD_POLICY_REGEX =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=[\]{};:'"\\|,.<>/?`~]).{8,}$/
+  /^(?=.*[a-z]) (?=.*[A-Z]) (?=.*\d) (?=.*[!@#$%^&*()_\-+=[\]{};:'"\\|,.<>/?`~])    .{8,}$/
 
 export function isStrongPassword(password: string) {
   return PASSWORD_POLICY_REGEX.test(password)
